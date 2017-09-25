d9d0c596aebaaca9c02c110fbe5de66396650a21
Cedric Ziel <cedric@cedric-ziel.com>
2017-09-25 10:10:43 +0200

    Enhance proc funcs with more detailed labels and shop retrieval

--- Classes/FormEngine/Utility/ProcFuncs.php
+++ Classes/FormEngine/Utility/ProcFuncs.php
@@ -2,8 +2,10 @@
 
 namespace DPN\SwConnect\FormEngine\Utility;
 
+use DPN\SwConnect\Domain\Model\Article;
 use DPN\SwConnect\Service\Decorator\CachedCategoryService;
 use DPN\SwConnect\Service\Decorator\CachedProductService;
+use DPN\SwConnect\Service\ShopService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 
@@ -36,8 +38,38 @@ class ProcFuncs
 
         $allProducts = $productService->findAll();
 
+        $products = [];
         foreach ($allProducts as $product) {
-            $fConfig['items'][] = [0 => $product->getName(), 1 => $product->getId()];
+            $products[] = $productService->findOne($product->getId());
+        }
+
+        foreach ($products as $product) {
+            /** @var Article $product */
+            $fConfig['items'][] = [0 => $product->getMainDetail()->getNumber().' - '.$product->getName(), 1 => $product->getId()];
+        }
+
+        usort($fConfig['items'], function($a, $b) {
+            return strcmp($a[0], $b[0]);
+        });
+    }
+
+    /**
+     * @param array $fConfig
+     * @param \TYPO3\CMS\Backend\Form\ $fObj
+     */
+    public static function getShops(&$fConfig, $fObj)
+    {
+        $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+        $shopService = $objectManager->get(ShopService::class);
+
+        $allShops = $shopService->findAll();
+
+        foreach ($allShops as $shop) {
+            $isDefault = (bool) $shop->isDefault();
+            $fConfig['items'][] = [
+                0 => sprintf('[%s] %s (%s) %s', $shop->getId(), $shop->getName(), $shop->getCurrency()->getName(), $isDefault ? 'Standard Shop' : ''),
+                1 => $shop->getId(),
+            ];
         }
     }
 }
