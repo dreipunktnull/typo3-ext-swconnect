f92a0deed097e578f01550ab94665f2a53318ceb
Cedric Ziel <cedric@cedric-ziel.com>
2018-05-03 12:41:43 +0200

    Display selected products in page layout view

--- Classes/Hook/PageLayoutView.php
+++ Classes/Hook/PageLayoutView.php
@@ -3,8 +3,10 @@
 namespace DPN\SwConnect\Hook;
 
 use DPN\SwConnect\FormEngine\Utility\TemplateLayout;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 
 /**
  * Renders the preview for an item.
@@ -34,11 +36,39 @@ class PageLayoutView
     public function printSettings(array $params) : string
     {
         $output = '';
-        $flexformData = GeneralUtility::xml2array($params['row']['pi_flexform']);
+        $flexFormData = GeneralUtility::xml2array($params['row']['pi_flexform']);
+        $settings = ObjectAccess::getPropertyPath($flexFormData, 'data.sDEF.lDEF');
+
+        $products = [];
+        if ($settings !== null && array_key_exists('settings.override.products', $settings)) {
+            $productIds = explode(',', $settings['settings.override.products']['vDEF']);
+
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+                ->getConnectionForTable('tx_dpnswconnect_article')
+                ->createQueryBuilder();
+            $products = $queryBuilder->select('shopware_id', 'name', 'article_number')
+                ->from('tx_dpnswconnect_article')
+                ->where($queryBuilder->expr()->in('shopware_id', $productIds))
+                ->execute()
+                ->fetchAll()
+            ;
+        }
 
         $output .= '<div class="table-responsive">';
         $output .= '<table class="table table-striped">';
-        $output .= '<tr><th>UID:</th><td>'.$params['row']['uid'].'</td></tr>';
+        $output .= '<tr><th>UID:</th><td>' . $params['row']['uid'] . '</td></tr>';
+        $output .= '<tr><th>Selected products:</th><td>';
+
+        if (\count($products) > 0) {
+            $output .= '<ul>';
+            foreach ($products as $product) {
+                $output .= '<li>' . sprintf('%s - %s (%s)', $product['article_number'], $product['name'], $product['shopware_id']) . '</li>';
+            }
+            $output .= '</ul>';
+        }
+
+        $output .= '</td></tr>';
+
         $output .= '</table>';
         $output .= '</div>';
 
